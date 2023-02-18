3b14adb3977c6d798a625bfb51265b075b70088a
KOTARO1209 <kmachida@mxdlab.net>
2023-02-19 04:46:05 +0900

    なべあつシステム完成

--- CountTest/ViewController.swift
+++ CountTest/ViewController.swift
@@ -21,10 +21,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func plus(){
+        
+        if number >= 40 {
+            return
+        }
+        
         number += 1
         label.text = String(number)
+        if number == 40 {
+            label.textColor = UIColor.red
+        }
         let str = String(number)
         
+        // 画像を描画する
+        let img01 = UIImageView(image: UIImage(named: "nabeatsu"))
+        if number > 30 {
+            img01.frame = CGRect(x: 30 * number - 870, y: 380, width: 454 / 10, height: 710 / 10)
+        } else {
+            img01.frame = CGRect(x: 10 * number + 20, y: 300, width: 454 / 10, height: 710 / 10)
+        }
+        self.view.addSubview(img01)
         
         if number % 3 == 0 && str.contains("3") {
             debugPrint("3の倍数かつ3を含んでいます")
@@ -33,12 +49,17 @@ class ViewController: UIViewController {
             debugPrint("3の倍数")
         } else if str.contains("3") {
             debugPrint("3を含んでいます")
-        } else if number == 40 {
-            debugPrint("終了")
+        } else {
+            debugPrint("その他の数字")
+            img01.isHidden = true
         }
+        
     }
     
     @IBAction func minus(){
+        if number >= 40 {
+            return
+        }
         number -= 1
         label.text = String(number)
     }
