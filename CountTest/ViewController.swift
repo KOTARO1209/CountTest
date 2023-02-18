45d2071f594c8461e14322873a84d06ad9f1fddb
KOTARO1209 <kmachida@mxdlab.net>
2023-02-19 03:58:06 +0900

    スクリプトからフォントが変わらない！

--- CountTest/ViewController.swift
+++ CountTest/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var number : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // フォントを定義する
+        label.font = UIFont(name: "yamafont", size: 90)
     }
 
-
+    @IBAction func plus(){
+        number += 1
+        label.text = String(number)
+        let str = String(number)
+        
+        
+        if number % 3 == 0 && str.contains("3") {
+            debugPrint("3の倍数かつ3を含んでいます")
+            return
+        } else if number % 3 == 0 {
+            debugPrint("3の倍数")
+        } else if str.contains("3") {
+            debugPrint("3を含んでいます")
+        } else if number == 40 {
+            debugPrint("終了")
+        }
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+    }
 }
 
