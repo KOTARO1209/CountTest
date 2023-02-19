fc818bab08beff8ca76d33f48a22526d4259a9da
KOTARO1209 <kmachida@mxdlab.net>
2023-02-19 22:40:48 +0900

    なべあつカウント完成！

--- CountTest/ViewController.swift
+++ CountTest/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // フォントを定義する
-        label.font = UIFont(name: "yamafont", size: 90)
+        label.font = UIFont(name: "Arial", size: 90)
     }
 
     @IBAction func plus(){
@@ -36,20 +36,24 @@ class ViewController: UIViewController {
         // 画像を描画する
         let img01 = UIImageView(image: UIImage(named: "nabeatsu"))
         if number > 30 {
-            img01.frame = CGRect(x: 30 * number - 870, y: 380, width: 454 / 10, height: 710 / 10)
+            img01.frame = CGRect(x: 30 * number - 880, y: 380, width: 454 / 10, height: 710 / 10)
         } else {
-            img01.frame = CGRect(x: 10 * number + 20, y: 300, width: 454 / 10, height: 710 / 10)
+            img01.frame = CGRect(x: 10 * number + 10, y: 300, width: 454 / 10, height: 710 / 10)
         }
         self.view.addSubview(img01)
         
         if number % 3 == 0 && str.contains("3") {
+            label.font = UIFont(name: "yamafont", size: 140)
             debugPrint("3の倍数かつ3を含んでいます")
             return
         } else if number % 3 == 0 {
+            label.font = UIFont(name: "yamafont", size: 140)
             debugPrint("3の倍数")
         } else if str.contains("3") {
+            label.font = UIFont(name: "yamafont", size: 140)
             debugPrint("3を含んでいます")
         } else {
+            label.font = UIFont(name: "Arial", size: 90)
             debugPrint("その他の数字")
             img01.isHidden = true
         }
@@ -62,6 +66,22 @@ class ViewController: UIViewController {
         }
         number -= 1
         label.text = String(number)
+        let str = String(number)
+        
+        if number % 3 == 0 && str.contains("3") {
+            label.font = UIFont(name: "yamafont", size: 140)
+            debugPrint("3の倍数かつ3を含んでいます")
+            return
+        } else if number % 3 == 0 {
+            label.font = UIFont(name: "yamafont", size: 140)
+            debugPrint("3の倍数")
+        } else if str.contains("3") {
+            label.font = UIFont(name: "yamafont", size: 140)
+            debugPrint("3を含んでいます")
+        } else {
+            label.font = UIFont(name: "Arial", size: 90)
+            debugPrint("その他の数字")
+        }
     }
 }
 
